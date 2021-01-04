e049b1626ac4a4a7fd98ad4d8f625eb11600d9d7
shihao wang <shihao.wang@hand-china.com>
2021-01-04 20:57:57 +0800

    feat: add -v param

--- bin/pub.js
+++ bin/pub.js
@@ -1,6 +1,8 @@
 const commander = require('commander');
 const { publish } = require('../lib');
 
+commander.version(require('../package.json').version, '-v --version')
+
 commander.option('--dist-tag <distTag>').action((command) => {
   const { distTag = 'latest' } = command;
   console.log('distTag', distTag);
